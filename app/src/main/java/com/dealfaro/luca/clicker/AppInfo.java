38331ea93d8a90964cf561cbde09d1510b7a313e
Francisco Rocha <frocha@ucsc.edu>
2015-05-11 21:41:21 -0700

    first commit

---
+++ app/src/main/java/com/dealfaro/luca/clicker/AppInfo.java
@@ -0,0 +1,7 @@
+package com.dealfaro.luca.clicker;
+
+/**
+ * Created by os_child2 on 5/5/15.
+ */
+public class AppInfo {
+}
---
+++ app/src/main/java/com/dealfaro/luca/clicker/Message.java
@@ -0,0 +1,7 @@
+package com.dealfaro.luca.clicker;
+
+/**
+ * Created by os_child2 on 4/26/15.
+ */
+public class Message {
+}
